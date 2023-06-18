c80b057beb0f37e73197a307d1cf1166bd91801a
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-06-18 04:22:29 -0300

    fix: transparent colors

--- src/make_flavours.py
+++ src/make_flavours.py
@@ -1,12 +1,14 @@
 from dataclasses import fields
 from pathlib import Path
 from string import Template
+from PIL import ImageColor
 
 from catppuccin import Flavour
 
 OUT_DIR = Path("build")
 OUT_DIR.mkdir(exist_ok=True)
 TEMPLATE_STR = Path("src/flavour-template.json").read_text()
+TRANSPARENCY = "7f"
 
 def palette():
     return {
@@ -16,49 +18,43 @@ def palette():
         } for flavour_method in [Flavour.latte, Flavour.frappe, Flavour.macchiato, Flavour.mocha]
     }
 
-def generate_color_with_transparency(color_hex: str) -> str:
-    # Extract RGB values from color_hex
-    r = int(color_hex[1:3], 16)
-    g = int(color_hex[3:5], 16)
-    b = int(color_hex[5:7], 16)
-    
-    # Generate the RGBA color string
-    rgba = f"rgba({r}, {g}, {b}, 0.5)"  # Change the alpha value (0.5) as desired
-    
-    return rgba
+def generate_colour_with_transparency(colour_value, transparency):
+    rgba_value = ImageColor.getrgb(colour_value)
+    rgba_value_with_transparency = rgba_value[:3] + (transparency,)
+    return f"#{''.join(format(c, '02x') for c in rgba_value_with_transparency)}"
 
 if __name__ == "__main__":
     template = Template(TEMPLATE_STR)
     for flavour, colours in palette().items():
-        # Determine the custom values based on the flavor
+        # Determine the custom values based on the flavour
         theme_base = "Light" if flavour.lower() == "latte" else "Dark"
         image_postfix = f"_{theme_base.lower()}"
         theme_name = f"Catppuccin {flavour.capitalize()}"
         transparent = "#00000000"
         plot_background = f"{colours['blue']}21"
 
-        # Create a new dictionary to store modified colors
-        modified_colours = dict(colours)
+        # Create a new dictionary to store transparent colours
+        transparent_colours = dict(colours)
 
-        # Generate color values with transparency
-        for color_name, color_value in colours.items():
-            if color_name.endswith("7f"):
-                # Skip colors that already have transparency
+        # Generate colour values with transparency
+        for colour_name, colour_value in colours.items():
+            if colour_name.endswith(TRANSPARENCY):
+                # Skip colours that already have transparency
                 continue
-            if color_value.startswith("#"):
-                # Generate color with transparency
-                color_with_transparency = generate_color_with_transparency(color_value)
-                modified_colours[color_name + "7f"] = color_with_transparency
+            if colour_value.startswith("#"):
+                # Generate colour with transparency
+                colour_with_transparency = generate_colour_with_transparency(colour_value, TRANSPARENCY)
+                transparent_colours[colour_name + TRANSPARENCY] = colour_with_transparency
 
-        # Add the custom values to the modified_colours dictionary
-        modified_colours["theme_base"] = theme_base
-        modified_colours["image_postfix"] = image_postfix
-        modified_colours["theme_name"] = theme_name
-        modified_colours["transparent"] = transparent
-        modified_colours["plot_background"] = plot_background
+        # Add the custom values to the transparent_colours dictionary
+        transparent_colours["theme_base"] = theme_base
+        transparent_colours["image_postfix"] = image_postfix
+        transparent_colours["theme_name"] = theme_name
+        transparent_colours["transparent"] = transparent
+        transparent_colours["plot_background"] = plot_background
 
-        # Substitute the template with the updated modified_colours dictionary
-        substituted_template = template.substitute(modified_colours)
+        # Substitute the template with the updated transparent_colours dictionary
+        substituted_template = template.substitute(transparent_colours)
 
         # Write the substituted template to the output file
         open(OUT_DIR / f"catppuccin-{flavour}.json", 'w').write(substituted_template)
