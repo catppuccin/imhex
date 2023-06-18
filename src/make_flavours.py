2e3554a6282315f5ab2e6cadac124126355eafdc
Doriel Rivalet <100863878+DorielRivalet@users.noreply.github.com>
2023-06-18 04:52:36 -0300

    fix: flavours generator

--- src/make_flavours.py
+++ src/make_flavours.py
@@ -1,14 +1,15 @@
 from dataclasses import fields
 from pathlib import Path
 from string import Template
-from PIL import ImageColor
 
 from catppuccin import Flavour
+from catppuccin import Colour
 
 OUT_DIR = Path("build")
 OUT_DIR.mkdir(exist_ok=True)
 TEMPLATE_STR = Path("src/flavour-template.json").read_text()
-TRANSPARENCY = "7f"
+TRANSPARENCY = 127
+TRANSPARENCY_HEX = format(TRANSPARENCY, "02x")  # Convert transparency to hex format
 
 def palette():
     return {
@@ -18,10 +19,11 @@ def palette():
         } for flavour_method in [Flavour.latte, Flavour.frappe, Flavour.macchiato, Flavour.mocha]
     }
 
-def generate_colour_with_transparency(colour_value, transparency):
-    rgba_value = ImageColor.getrgb(colour_value)
-    rgba_value_with_transparency = rgba_value[:3] + (transparency,)
-    return f"#{''.join(format(c, '02x') for c in rgba_value_with_transparency)}"
+def generate_colour_with_transparency(colour_hex: Colour, alpha: int) -> str:
+    """Generate a colour string with transparency."""
+    colour = Colour.from_hex(colour_hex)
+    colour_with_transparency = Colour(colour.red, colour.green, colour.blue, alpha)
+    return "#" + colour_with_transparency.hex
 
 if __name__ == "__main__":
     template = Template(TEMPLATE_STR)
@@ -38,13 +40,13 @@ if __name__ == "__main__":
 
         # Generate colour values with transparency
         for colour_name, colour_value in colours.items():
-            if colour_name.endswith(TRANSPARENCY):
+            if colour_name.endswith(TRANSPARENCY_HEX):
                 # Skip colours that already have transparency
                 continue
             if colour_value.startswith("#"):
                 # Generate colour with transparency
-                colour_with_transparency = generate_colour_with_transparency(colour_value, TRANSPARENCY)
-                transparent_colours[colour_name + TRANSPARENCY] = colour_with_transparency
+                colour_with_transparency = generate_colour_with_transparency(colour_value.strip("#"), TRANSPARENCY)
+                transparent_colours[colour_name + TRANSPARENCY_HEX] = colour_with_transparency
 
         # Add the custom values to the transparent_colours dictionary
         transparent_colours["theme_base"] = theme_base
